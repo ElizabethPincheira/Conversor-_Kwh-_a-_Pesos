6ceca670ff1ac570e76a437e77b38887b2eb35d1
elizabeth <elizabeth.pincheira.caro@gmail.com>
2024-12-25 22:17:15 -0300

    se avanza con navbar

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonMenu } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  styleUrls: ['./navbar.component.scss', ],
+  imports: [IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonMenuButton]
 })
 export class NavbarComponent  implements OnInit {
 
