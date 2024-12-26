b2fd773a614f1d83fff57f9ef32b749458561764
elizabeth <elizabeth.pincheira.caro@gmail.com>
2024-12-26 19:18:29 -0300

    pantallas hechas

--- src/app/paginas/config/config.page.ts
+++ src/app/paginas/config/config.page.ts
@@ -1,21 +1,51 @@
+// IMPORTACIONES
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent, IonCard, IonCardHeader,
+  IonCardTitle, IonCardSubtitle, IonCardContent, IonItem, IonInput, IonButton
+} from '@ionic/angular/standalone';
 import { NavbarComponent } from 'src/app/componentes/navbar/navbar.component';
 
+// DECORADOR @Component
 @Component({
   selector: 'app-config',
   templateUrl: './config.page.html',
   styleUrls: ['./config.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent]
+  imports: [IonContent, CommonModule,
+    FormsModule, NavbarComponent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent,
+    IonItem, IonInput, IonButton]
 })
+
+// CLASE ConfigPage
 export class ConfigPage implements OnInit {
+
+  // datos de consumo
+  kwhConsumoIngresadoBoleta :number = 0
+  precioConsumoIngresado :number = 0
+
+  // datos de aporte
+  kwhAporteIngresadoBoleta :number = 0
+  precioAporteIngresado :number = 0
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
+   obtenerPrecios(kwh: number, precio: number) {
+    return  precio/kwh;
+  }
+
+  guardarConfiguracion(){
+
+    localStorage.setItem("precioCompra", this.obtenerPrecios(this.kwhConsumoIngresadoBoleta, this.precioConsumoIngresado) + "");
+    localStorage.setItem("precioVenta", this.obtenerPrecios(this.kwhAporteIngresadoBoleta, this.precioAporteIngresado) + "");
+
+  }
+
 
 }
--- src/app/paginas/home/home.page.ts
+++ src/app/paginas/home/home.page.ts
@@ -1,13 +1,58 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent,IonCard,IonCardHeader,
+  IonCardTitle,IonCardContent,IonItem,IonInput,IonButton } from '@ionic/angular/standalone';
 import { NavbarComponent } from 'src/app/componentes/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NavbarComponent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonContent, CommonModule, 
+    FormsModule, NavbarComponent,IonCard,IonCardHeader,IonCardTitle,IonCardContent,
+    IonItem,IonInput,IonButton],
 })
 export class HomePage {
+
   constructor() {}
+
+  // ******VARIABLES*****
+
+totalPagar : number | null = null;
+
+  // datos ingresados
+kwhUtilizadosMedidor : number = 0
+kwhGeneradosMedidor : number = 0
+
+// precios localstore
+// precio compra
+precioDeCompra : any = localStorage.getItem("precioCompra");
+numeroPrecioDeCompra : number | null = parseFloat(this.precioDeCompra);
+
+// precio compra
+precioDeventa : any = localStorage.getItem("precioVenta");
+numeroPrecioDeVenta : number | null = parseFloat(this.precioDeventa);
+
+
+calcularValores( kwh : number, precio : number){
+  return kwh * precio
 }
+
+calcular(){
+  console.log("boton")
+  this.totalPagar = 0;
+
+  console.log(this.totalPagar)
+}
+
+
+
+  
+
+
+
+
+}
+
